535a8da3e33d65a7d0f1274e697a46d7342028b7
Saurabh Singh <sureshsingh09120@gmail.com>
2025-05-18 19:40:56 +0530

    Added Homepage

---
+++ frontend/src/components/pages/Services.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Services() {
+  return <h1 className='services'>SERVICES</h1>;
+}
+// The Services component is a simple functional component that renders a heading with the text "SERVICES".
