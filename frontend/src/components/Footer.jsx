e7d0dd5748ab6a2b5e2015111a8f25406f138c9f
Srinath0916 <srinath0916@gmail.com>
2025-05-20 01:03:21 +0530

    Added Footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,62 +1,25 @@
 import React from 'react';
 import './Footer.css';
-import { Button } from './Button';
 import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
-          Join the Adventure newsletter to receive our best vacation deals
-        </p>
-        <p className='footer-subscription-text'>
-          You can unsubscribe at any time.
-        </p>
-        <div className='input-areas'>
-          <form>
-            <input
-              className='footer-input'
-              name='email'
-              type='email'
-              placeholder='Your Email'
-            />
-            <Button buttonStyle='btn--outline'>Subscribe</Button>
-          </form>
-        </div>
-      </section>
       <div className='footer-links'>
         <div className='footer-link-wrapper'>
           <div className='footer-link-items'>
-            <h2>About Us</h2>
-            <Link to='/sign-up'>How it works</Link>
-            <Link to='/'>Testimonials</Link>
-            <Link to='/'>Careers</Link>
-            <Link to='/'>Investors</Link>
+            <h2>Quick Links</h2>
+            <Link to='/cities'>Cities</Link>
+            <Link to='/explore'>Destinations</Link>
+            <Link to='/'>Popular Places</Link>
+            <Link to='/'>Travel Packages</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Support</h2>
+            <Link to='/'>Contact Us</Link>
+            <Link to='/'>FAQs</Link>
+            <Link to='/'>Privacy Policy</Link>
             <Link to='/'>Terms of Service</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Destinations</Link>
-            <Link to='/'>Sponsorships</Link>
-          </div>
-        </div>
-        <div className='footer-link-wrapper'>
-          <div className='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div className='footer-link-items'>
-            <h2>Social Media</h2>
-            <Link to='/'>Instagram</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            <Link to='/'>Twitter</Link>
           </div>
         </div>
       </div>
@@ -68,16 +31,8 @@ function Footer() {
               <img src="/images/logo.png" alt="Destinex Logo" className="footer-logo-img" />
             </Link>
           </div>
-          <small className='website-rights'>Destinex© 2025</small>
+          <small className='website-rights'>Destinex © {new Date().getFullYear()}</small>
           <div className='social-icons'>
-            <Link
-              className='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i className='fab fa-facebook-f' />
-            </Link>
             <Link
               className='social-icon-link instagram'
               to='/'
@@ -87,12 +42,12 @@ function Footer() {
               <i className='fab fa-instagram' />
             </Link>
             <Link
-              className='social-icon-link youtube'
+              className='social-icon-link facebook'
               to='/'
               target='_blank'
-              aria-label='Youtube'
+              aria-label='Facebook'
             >
-              <i className='fab fa-youtube' />
+              <i className='fab fa-facebook-f' />
             </Link>
             <Link
               className='social-icon-link twitter'
@@ -101,14 +56,6 @@ function Footer() {
               aria-label='Twitter'
             >
               <i className='fab fa-twitter' />
-            </Link>
-            <Link
-              className='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i className='fab fa-linkedin' />
             </Link>
           </div>
         </div>
